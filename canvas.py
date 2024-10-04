837d3739419e3a699766b3cac07222700aed27e7
id0ntexist <mahjabinzara2013>
2024-10-04 13:53:40 +0600

    intial commit

---
+++ canvas.py
@@ -0,0 +1,11 @@
+number= int(input("Enter number to check"))
+print("number to be checked :",number)
+
+if number>0:
+    print("this is a positive number")
+
+elif number<0:
+    print("this is a positive number")
+
+else:
+    print("this is neutral")
